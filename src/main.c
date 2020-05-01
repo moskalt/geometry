5e21f8149664424b866db9de4808deffc9777246
moskalt <thetima2@gmail.com>
2020-05-01 17:57:43 +0700

    Format fix

--- src/main.c
+++ src/main.c
@@ -4,27 +4,28 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main() {
-  const int MAX_LEN = 100;
-  char **temp_data = (char **)calloc(MAX_LEN, sizeof(char *));
-  for (int i = 0; i < MAX_LEN; i++) {
-    temp_data[i] = (char *)calloc(MAX_LEN, sizeof(char));
-  }
-  char **file_data = (char **)calloc(MAX_LEN, sizeof(char *));
-  for (int i = 0; i < MAX_LEN; i++) {
-    file_data[i] = (char *)calloc(MAX_LEN, sizeof(char));
-  }
-  for (int i = 0; i < MAX_LEN; i++) {
-    for (int j = 0; j < MAX_LEN; j++) {
-      file_data[i][j] = 0;
+int main()
+{
+    const int MAX_LEN = 100;
+    char** temp_data = (char**)calloc(MAX_LEN, sizeof(char*));
+    for (int i = 0; i < MAX_LEN; i++) {
+        temp_data[i] = (char*)calloc(MAX_LEN, sizeof(char));
     }
-  }
-  temp_data = file_read_function(file_data);
-  for (int i = 0; i < MAX_LEN; i++) {
-    if (temp_data[i] != 0) {
-      printf("%s", temp_data[i]);
+    char** file_data = (char**)calloc(MAX_LEN, sizeof(char*));
+    for (int i = 0; i < MAX_LEN; i++) {
+        file_data[i] = (char*)calloc(MAX_LEN, sizeof(char));
     }
-  }
-  printf("\n");
-  return 0;
+    for (int i = 0; i < MAX_LEN; i++) {
+        for (int j = 0; j < MAX_LEN; j++) {
+            file_data[i][j] = 0;
+        }
+    }
+    temp_data = file_read_function(file_data);
+    for (int i = 0; i < MAX_LEN; i++) {
+        if (temp_data[i] != 0) {
+            printf("%s", temp_data[i]);
+        }
+    }
+    printf("\n");
+    return 0;
 }
