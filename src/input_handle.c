33acbb7d54ecfb9c047f307d755d120695cac1d6
moskalt <thetima2@gmail.com>
2020-05-01 17:54:22 +0700

    Format fix

--- src/input_handle.c
+++ src/input_handle.c
@@ -3,53 +3,59 @@
 #include <stdlib.h>
 #include <string.h>
 
-int name_check(char *str) {
-  const char triangle[] = "triangle";
-  const char caps_triangle[] = "TRIANGLE";
-  const char circle[] = "circle";
-  const char caps_circle[] = "CIRCLE";
-  int check = 1;
-  if (str[0] == 'c' || str[0] == 'C') {
-    for (int i = 0; str[i] != '('; i++) {
-      if (str[i] != triangle[i] && str[i] != caps_triangle[i]) {
-        check = 0;
-      }
+int name_check(char* str)
+{
+    const char triangle[] = "triangle";
+    const char caps_triangle[] = "TRIANGLE";
+    const char circle[] = "circle";
+    const char caps_circle[] = "CIRCLE";
+    int check = 1;
+    if (str[0] == 'c' || str[0] == 'C') {
+        for (int i = 0; str[i] != '('; i++) {
+            if (str[i] != triangle[i] && str[i] != caps_triangle[i]) {
+                check = 0;
+            }
+        }
+    } else if (str[0] == 't' || str[0] == 'T') {
+        for (int i = 0; str[i] != '('; i++) {
+            if (str[i] != circle[i] && str[i] != caps_circle[i]) {
+                check = 0;
+            }
+        }
     }
-  } else if (str[0] == 't' || str[0] == 'T') {
-    for (int i = 0; str[i] != '('; i++) {
-      if (str[i] != circle[i] && str[i] != caps_circle[i]) {
-        check = 0;
-      }
-    }
-  }
-  if (check == 1) {
-    return 1;
-  } else
-    return 0;
+    if (check == 1) {
+        return 1;
+    } else
+        return 0;
 }
 
-char **file_read_function(char **file_data) {
-  FILE *myfile;
-  myfile = fopen("./example/target.txt", "r");
-  if (myfile == NULL) {
-    printf("Error while opening file");
-    return 1;
-  }
-  int i = 0;
-  char *temp_string, *ptrFile;
-  while (1) {
-    temp_string = (char *)calloc(1000, sizeof(char));
-    ptrFile = fgets(temp_string, 1000, myfile);
-    file_data[i] = temp_string;
-    i++;
-    if (ptrFile == NULL) {
-      if (feof(myfile) != 0) {
-        break;
-      } else {
-        continue;
-      }
+char** file_read_function(char** file_data)
+{
+    FILE* myfile;
+    myfile = fopen("./example/target.txt", "r");
+    if (myfile == NULL) {
+        printf("Error while opening file");
+        return 1;
     }
-  }
-  fclose(myfile);
-  return file_data;
+    int i = 0;
+    char *temp_string, *ptrFile;
+    while (1) {
+        temp_string = (char*)calloc(1000, sizeof(char));
+        ptrFile = fgets(temp_string, 1000, myfile);
+        if (name_check(temp_string) == 0) {
+            printf("Wrong figure's name");
+            return 0;
+        }
+        file_data[i] = temp_string;
+        i++;
+        if (ptrFile == NULL) {
+            if (feof(myfile) != 0) {
+                break;
+            } else {
+                continue;
+            }
+        }
+    }
+    fclose(myfile);
+    return file_data;
 }
