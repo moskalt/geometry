3b2ca94eacfb8d31415e91ac1737eed8e67ab8a6
moskalt <thetima2@gmail.com>
2020-05-01 16:19:03 +0700

    Functrion replaced from main to input_handle module

--- src/input_handle.c
+++ src/input_handle.c
@@ -1,1 +1,30 @@
 #include "input_handle.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char **file_read_function(char **file_data) {
+  FILE *myfile;
+  myfile = fopen("./target.txt", "r");
+  if (myfile == NULL) {
+    printf("Error while opening file");
+    return 1;
+  }
+  int i = 0;
+  char *temp_string, *ptrFile;
+  while (1) {
+    temp_string = (char *)calloc(1000, sizeof(char));
+    ptrFile = fgets(temp_string, 1000, myfile);
+    file_data[i] = ptrFile;
+    i++;
+    if (ptrFile == NULL) {
+      if (feof(myfile) != 0) {
+        break;
+      } else {
+        continue;
+      }
+    }
+  }
+  fclose(myfile);
+  return file_data;
+}
--- src/main.c
+++ src/main.c
@@ -1,38 +1,8 @@
+#include "input_handle.h"
+#include "intersection.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-struct first_handle {
-  char string[40];
-} ex;
-
-int string_counter() {}
-
-char **file_read_function(char **file_data) {
-  FILE *myfile;
-  myfile = fopen("./target.txt", "r");
-  if (myfile == NULL) {
-    printf("Error while opening file");
-    return 1;
-  }
-  int i = 0;
-  char *temp_string, *ptrFile;
-  while (1) {
-    temp_string = (char *)calloc(1000, sizeof(char));
-    ptrFile = fgets(temp_string, 1000, myfile);
-    file_data[i] = ptrFile;
-    i++;
-    if (ptrFile == NULL) {
-      if (feof(myfile) != 0) {
-        break;
-      } else {
-        continue;
-      }
-    }
-  }
-  fclose(myfile);
-  return file_data;
-}
 
 int main() {
   const int MAX_LEN = 100;
