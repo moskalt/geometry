f1afc8c932cf5c2686a539268f9ab605e75e4e0b
moskalt <thetima2@gmail.com>
2020-05-01 15:54:47 +0700

    Format fix

--- src/main.c
+++ src/main.c
@@ -3,63 +3,58 @@
 #include <string.h>
 
 struct first_handle {
-    char string[40];
+  char string[40];
 } ex;
 
-int string_counter(){
+int string_counter() {}
 
+char **file_read_function(char **file_data) {
+  FILE *myfile;
+  myfile = fopen("./target.txt", "r");
+  if (myfile == NULL) {
+    printf("Error while opening file");
+    return 1;
+  }
+  int i = 0;
+  char *temp_string, *ptrFile;
+  while (1) {
+    temp_string = (char *)malloc(1000 * sizeof(char));
+    ptrFile = fgets(temp_string, 1000, myfile);
+    file_data[i] = ptrFile;
+    i++;
+    if (ptrFile == NULL) {
+      if (feof(myfile) != 0) {
+        break;
+      } else {
+        continue;
+      }
+    }
+  }
+  fclose(myfile);
+  return file_data;
 }
 
-
-char** file_read_function(char** file_data)
-{
-    FILE* myfile;
-    myfile = fopen("./target.txt", "r");
-    if (myfile == NULL) {
-        printf("Error while opening file");
-        return 1;
+int main() {
+  const int MAX_LEN = 100;
+  char **temp_data = (char **)malloc(MAX_LEN * sizeof(char *));
+  for (int i = 0; i < MAX_LEN; i++) {
+    temp_data[i] = (char *)malloc(MAX_LEN * sizeof(char));
+  }
+  char **file_data = (char **)malloc(MAX_LEN * sizeof(char *));
+  for (int i = 0; i < MAX_LEN; i++) {
+    file_data[i] = (char *)malloc(MAX_LEN * sizeof(char));
+  }
+  for (int i = 0; i < MAX_LEN; i++) {
+    for (int j = 0; j < MAX_LEN; j++) {
+      file_data[i][j] = 0;
     }
-    int i = 0;
-    char *temp_string, *ptrFile;
-    while (1) {
-        temp_string = (char*)malloc(1000 * sizeof(char));
-        ptrFile = fgets(temp_string, 1000, myfile);
-        file_data[i] = ptrFile;
-        i++;
-        if (ptrFile == NULL) {
-            if (feof(myfile) != 0) {
-                break;
-            } else {
-                continue;
-            }
-        }
-    }
-    fclose(myfile);
-    return file_data;
-}
-
-int main()
-{
-    const int MAX_LEN = 100;
-    char** temp_data = (char**)malloc(MAX_LEN*sizeof(char*));
-    for(int i = 0; i < MAX_LEN; i++){
-        temp_data[i] = (char*)malloc(MAX_LEN*sizeof(char));
-    }
-    char** file_data = (char**)malloc(MAX_LEN * sizeof(char*));
-    for (int i = 0; i < MAX_LEN; i++) {
-        file_data[i] = (char*)malloc(MAX_LEN * sizeof(char));
-    }
-    for (int i = 0; i < MAX_LEN; i++) {
-        for (int j = 0; j < MAX_LEN; j++) {
-            file_data[i][j] = 0;
-        }
-    }
-    temp_data = file_read_function(file_data);
-    for(int i = 0;i < MAX_LEN; i++){
-        free(temp_data[i]);
-        free(file_data[i]);
-    }
-    free(temp_data);
-    free(file_data);
-    return 0;
+  }
+  temp_data = file_read_function(file_data);
+  for (int i = 0; i < MAX_LEN; i++) {
+    free(temp_data[i]);
+    free(file_data[i]);
+  }
+  free(temp_data);
+  free(file_data);
+  return 0;
 }
