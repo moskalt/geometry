0e625cec6c3246b52d49a4e3b331ce00f0b2ca33
moskalt <thetima2@gmail.com>
2020-05-05 17:08:25 +0700

    Added struct filling in main.c

--- src/main.c
+++ src/main.c
@@ -33,23 +33,38 @@ int main()
         }
     }
     temp_data = file_read_function(file_data);
-    for (int i = 0; i < MAX_LEN; i++) {
-        if (temp_data[i] != 0) {
-            printf("%s", temp_data[i]);
+    for (int i = 0; i < MAX_COUNT_OF_SHAPES; i++) {
+        if (temp_data[i][0] != '\0') {
+            if (get_name(temp_data[i]) == 1) {
+                shapes_array[i].name = "triangle";
+                shapes_array[i].type = 1;
+                shapes_array[i].coordinates = coord_to_int(temp_data[i]);
+            } else if (get_name(temp_data[i]) == 2) {
+                shapes_array[i].name = "circle";
+                shapes_array[i].type = 2;
+                shapes_array[i].coordinates = coord_to_int(temp_data[i]);
+            }
         }
     }
-    char* temp_string = temp_data[0];
-    char** array = coordinates_separation(temp_string, get_name(temp_string));
-    printf("\n");
-    for (int i = 0; i < 4; i++) {
-        printf("%s\n", array[i]);
+    for (int i = 0; i < MAX_COUNT_OF_SHAPES; i++) {
+        if (shapes_array[i].type == 0) {
+            continue;
+        }
+        printf("\n name - %s\n", shapes_array[i].name);
+        if (shapes_array[i].type == 1) {
+            printf("   coordinates :{");
+            for (int j = 0; j < 8; j++) {
+                printf("%d  ", shapes_array[i].coordinates[j]);
+            }
+            printf("}");
+        } else if (shapes_array[i].type == 2) {
+            printf("   coordinates :{");
+            for (int j = 0; j < 3; j++) {
+                printf("%d  ", shapes_array[i].coordinates[j]);
+            }
+            printf("}");
+        }
+        printf("\n");
     }
-    printf("\n");
-
-    int* array_int = coord_to_int(temp_string);
-    for (int i = 0; i < 8; i++) {
-        printf("%d ", array_int[i]);
-    }
-    printf("\n");
     return 0;
 }
