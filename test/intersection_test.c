f0b081c0aa04f4c5b36f9120889c20f716c3c6a5
moskalt <thetima2@gmail.com>
2020-05-05 18:24:45 +0700

    Added tests for other simple functions

--- test/intersection_test.c
+++ test/intersection_test.c
@@ -15,3 +15,35 @@ CTEST(intersection, perimeter)
     ASSERT_EQUAL(exception_first, res_first);
     ASSERT_EQUAL(exception_fourth, res_fourth);
 }
+
+CTEST(intersection, perimeter_circle)
+{
+    // Given
+    int first[] = {0, 0, 12};
+    // When
+    float res_first = perimeter_circle(first);
+    // Then
+    float exception_first = 75.398224;
+    ASSERT_EQUAL(exception_first, res_first);
+}
+
+CTEST(intersection, area_triangle)
+{
+    // Given
+    int first[] = {3, -2, 0, 0, -3, 5, 3, -2};
+    // When
+    float res_first = area_triangle(first);
+    // Then
+    float exception_first = 4.500003;
+    ASSERT_EQUAL(exception_first, res_first);
+}
+CTEST(intersection, area_circle)
+{
+    // Given
+    int first[] = {0, 0, 12};
+    // When
+    float res_first = area_circle(first);
+    // Then
+    float exception_first = 452.389343;
+    ASSERT_EQUAL(exception_first, res_first);
+}
